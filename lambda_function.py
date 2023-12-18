73ff3134bb6aeee2adb8aaaca9d830f395d4dabe
charles-peykov <charles.peykov@gmail.com>
2023-12-18 17:00:53 +0200

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,10 @@
+import json
+from datetime import datetime
+
+def handler(event, context):
+  print("Function called")
+  current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+  return {
+      'statusCode': 200,
+      'body': json.dumps('Hello World! Current time: ' + current_time)
+  }
